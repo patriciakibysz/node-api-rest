27917e401b74918aeeb63b6bfa67521c290a21de
patriciakibysz <patriciakibysz@gmail.com>
2025-07-25 22:44:46 -0300

    Get Product By Id

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,6 +1,6 @@
 import { db } from "./firebase.js";
 
-import {collection, getDocs } from "firebase/firestore";
+import {collection, getDocs, doc, getDoc } from "firebase/firestore";
 
 const productsCollection = collection(db, "products");
 
@@ -15,4 +15,18 @@ export const getAllProducts = async () => {
     } catch (error) {
         console.error(error);
     }
+};
+
+export const getProductById = async (id) => {
+  try {
+    const docRef = doc(productsCollection, id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return { id: docSnap.id, ...docSnap.data() };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
--- src/services/products.service.js
+++ src/services/products.service.js
@@ -3,3 +3,7 @@ import * as Model from "../models/products.model.js";
 export const getAllProducts = async () => {
     return await Model.getAllProducts();
 };
+
+export const getProductById = async (id) => {
+  return await Model.getProductById(id);
+};
